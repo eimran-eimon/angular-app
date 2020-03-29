e790fdf2313ffb6262a4edc17ba993317a946d33
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-03-29 15:54:48 +0600

    Initialized for project (recipe-shopping-list)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
