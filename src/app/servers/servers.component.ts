8841add1b46b054850c65b364fd279e840beb60f
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-03-29 13:58:39 +0600

    bi-directional data binding.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server created!';
-  serverName = '';
+  serverName = 'Server Name';
   constructor() {
     setTimeout(() => {
         this.allowNewServer = true;
