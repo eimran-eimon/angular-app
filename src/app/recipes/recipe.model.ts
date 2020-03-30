6a7221e52f39fe6c90666224e97188f951df8b4b
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-03-30 11:15:05 +0600

    shopping list component added.

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -3,10 +3,12 @@ export class Recipe {
   public description: string;
   public imagePath: string;
 
-  constructor(name: string, description: string, imagePath: string ) {
+
+  constructor(name: string, description: string, imagePath: string) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.name = name;
+    // later: field name should be deleted.
   }
-
 }
