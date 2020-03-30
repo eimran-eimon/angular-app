cca1aeeb35cdf112da9d1be3ac27c2768dc3fdda
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-03-30 15:49:21 +0600

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  feature = 'recipe';
+
+  navigate(featureName: string) {
+    this.feature = featureName;
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('nameAmount') amount: ElementRef;
+  @Output() addIngredientEvent = new EventEmitter<Ingredient>()
+  private ingredientAmount: number;
+  private ingredientName: string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addIngredient() {
+    // console.log(this.ingredientAmount.nativeElement.value + '' + this.ingredientName.nativeElement.value);
+    this.ingredientAmount = this.amount.nativeElement.value;
+    this.ingredientName = this.name.nativeElement.value;
+    this.addIngredientEvent.emit(new Ingredient(this.ingredientName, this.ingredientAmount));
+  }
 }
