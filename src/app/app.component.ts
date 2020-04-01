b394442a90d9a1439577b10dbe77e371d94ea7da
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-04-01 19:17:48 +0600

    Template driven form completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultQuestion = 'pet';
+  reply: string;
+  genders = ['Male', 'Female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // LATER: USE signUpForm.setValue() for whole form
+    this.signUpForm.form.patchValue(
+      {
+        userData: {
+          username: suggestedName
+        }
+      }
+    );
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+    this.signUpForm.reset();
   }
 }
